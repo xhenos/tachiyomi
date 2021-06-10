09073fc6165bb83fffc265bf29875fade5f35109
Ivan Iskandar <iiiiskandar14@gmail.com>
2021-06-11 00:20:28 +0700

    [WIP] Tracking sheet visual adjustments

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackAdapter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackAdapter.kt
@@ -1,9 +1,15 @@
 package eu.kanade.tachiyomi.ui.manga.track
 
+import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.elevation.ElevationOverlayProvider
+import eu.kanade.tachiyomi.data.preference.PreferenceValues
+import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.databinding.TrackItemBinding
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class TrackAdapter(listener: OnClickListener) : RecyclerView.Adapter<TrackHolder>() {
 
@@ -29,6 +35,22 @@ class TrackAdapter(listener: OnClickListener) : RecyclerView.Adapter<TrackHolder
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
         binding = TrackItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        val ctx = binding.card.context
+        val isDarkMode = when (Injekt.get<PreferencesHelper>().themeMode().get()) {
+            PreferenceValues.ThemeMode.light -> false
+            PreferenceValues.ThemeMode.dark -> true
+            PreferenceValues.ThemeMode.system -> ctx.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+        }
+        // TODO: Don't.
+        if (isDarkMode) {
+            binding.card.setCardBackgroundColor(
+                ElevationOverlayProvider(ctx).compositeOverlay(
+                    binding.card.cardBackgroundColor.defaultColor,
+                    binding.card.cardElevation
+                )
+            )
+        }
         return TrackHolder(binding, this)
     }
 
--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackHolder.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/track/TrackHolder.kt
@@ -3,12 +3,13 @@ package eu.kanade.tachiyomi.ui.manga.track
 import android.annotation.SuppressLint
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.preference.PreferencesHelper
 import eu.kanade.tachiyomi.databinding.TrackItemBinding
 import uy.kohesive.injekt.injectLazy
 import java.text.DateFormat
 
-class TrackHolder(private val binding: TrackItemBinding, adapter: TrackAdapter) : RecyclerView.ViewHolder(binding.root) {
+class TrackHolder(private val binding: TrackItemBinding, private val adapter: TrackAdapter) : RecyclerView.ViewHolder(binding.root) {
 
     private val preferences: PreferencesHelper by injectLazy()
 
@@ -37,34 +38,51 @@ class TrackHolder(private val binding: TrackItemBinding, adapter: TrackAdapter) 
     fun bind(item: TrackItem) {
         val track = item.track
         binding.trackLogo.setImageResource(item.service.getLogo())
-        binding.logoContainer.setBackgroundColor(item.service.getLogoColor())
+        binding.logoContainer.setCardBackgroundColor(item.service.getLogoColor())
 
         binding.trackSet.isVisible = track == null
         binding.trackTitle.isVisible = track != null
 
-        binding.topDivider.isVisible = track != null
         binding.middleRow.isVisible = track != null
         binding.bottomDivider.isVisible = track != null
         binding.bottomRow.isVisible = track != null
 
+        binding.itemDivider.isVisible = adapter.items.indexOf(item) != 0
+        binding.card.isVisible = track != null
+
         if (track != null) {
+            val ctx = binding.trackTitle.context
             binding.trackTitle.text = track.title
             binding.trackChapters.text = "${track.last_chapter_read}/" +
-                if (track.total_chapters > 0) track.total_chapters else "-"
+                if (track.total_chapters > 0) track.total_chapters else ctx.getString(R.string.chapters)
             binding.trackStatus.text = item.service.getStatus(track.status)
 
             if (item.service.getScoreList().isEmpty()) {
                 binding.trackScore.isVisible = false
                 binding.vertDivider2.isVisible = false
             } else {
-                binding.trackScore.text = if (track.score == 0f) "-" else item.service.displayScore(track)
+                binding.trackScore.text = if (track.score != 0f) {
+                    item.service.getScoreList()
+                    item.service.displayScore(track)
+                } else {
+                    binding.trackScore.alpha = .5F
+                    ctx.getString(R.string.score)
+                }
             }
 
             if (item.service.supportsReadingDates) {
-                binding.trackStartDate.text =
-                    if (track.started_reading_date != 0L) dateFormat.format(track.started_reading_date) else "-"
-                binding.trackFinishDate.text =
-                    if (track.finished_reading_date != 0L) dateFormat.format(track.finished_reading_date) else "-"
+                binding.trackStartDate.text = if (track.started_reading_date != 0L) {
+                    dateFormat.format(track.started_reading_date)
+                } else {
+                    binding.trackStartDate.alpha = .5F
+                    ctx.getString(R.string.track_started_reading_date)
+                }
+                binding.trackFinishDate.text = if (track.finished_reading_date != 0L) {
+                    dateFormat.format(track.finished_reading_date)
+                } else {
+                    binding.trackFinishDate.alpha = .5F
+                    ctx.getString(R.string.track_finished_reading_date)
+                }
             } else {
                 binding.bottomDivider.isVisible = false
                 binding.bottomRow.isVisible = false
